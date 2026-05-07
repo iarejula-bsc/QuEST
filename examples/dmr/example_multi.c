e1ea230d15af3cbd93ebfad7d8b365c97e661229
Íñigo Aréjula Aísa <inigo.arejula@bsc.es>
2026-05-07 14:33:18 +0200

    dmr: make example_multi circuits dynamics-based

--- examples/dmr/example_multi.c
+++ examples/dmr/example_multi.c
@@ -7,121 +7,99 @@
  */
 #include "quest.h"
 #include "dmr.h"
+#include <stdio.h>
 
-// Circuit with 2 qubits
-void circuit_2_qubits() {
+static PauliStrSum create_heisenberg_ring_hamiltonian(int numQubits) {
+  char *operators[] = {"XX", "YY", "ZZ", "Z"};
+  qcomp coefficients[] = {.1, .2, .3, .4};
 
-  Qureg qureg = createQureg(2);
-  reportQuregParams(qureg);
+  int numTerms = 4 * numQubits;
+  PauliStr allStrings[256];
+  qcomp allCoeffs[256];
+  int allInd = 0;
 
-  DMRAnalytics *analytics_start;
-  dmr_create_custom_analytics_event("circuit_start", &analytics_start);
-  dmr_print_analytics_from(analytics_start);
-  dmr_destroy_custom_analytics_event(analytics_start);
+  for (int p = 0; p < 3; p++) {
+    for (int i = 0; i < numQubits; i++) {
+      int targs[] = {i, (i + 1) % numQubits};
+      allStrings[allInd] = getPauliStr(operators[p], targs, 2);
+      allCoeffs[allInd] = coefficients[p];
+      allInd++;
+    }
+  }
 
-  initRandomPureState(qureg);
-  reportQureg(qureg);
-  qreal prob = calcTotalProb(qureg);
-  reportScalar("Total probability (2 qubits)", prob);
+  for (int i = 0; i < numQubits; i++) {
+    allStrings[allInd] = getPauliStr(operators[3], &i, 1);
+    allCoeffs[allInd] = coefficients[3];
+    allInd++;
+  }
 
-  DMRAnalytics *analytics_end;
-  dmr_create_custom_analytics_event("circuit_end", &analytics_end);
-  dmr_print_analytics_from(analytics_end);
-  dmr_destroy_custom_analytics_event(analytics_end);
-  destroyQureg(qureg);
+  return createPauliStrSum(allStrings, allCoeffs, numTerms);
 }
 
-// Circuit with 3 qubits
-void circuit_3_qubits() {
+static PauliStrSum create_alternating_observable(int numQubits) {
+  PauliStr strings[64];
+  qcomp coeffs[64];
 
-  Qureg qureg = createQureg(3);
-  reportQuregParams(qureg);
-  
-  DMRAnalytics *analytics_start;
-  dmr_create_custom_analytics_event("circuit_start", &analytics_start);
-  dmr_print_analytics_from(analytics_start);
-  dmr_destroy_custom_analytics_event(analytics_start);
+  for (int i = 0; i < numQubits; i++) {
+    char pauli = "XYZ"[i % 3];
+    strings[i] = getPauliStr(&pauli, &i, 1);
+    coeffs[i] = getQcomp((i % 4) + 1, 0);
+  }
 
-  initRandomPureState(qureg);
-  reportQureg(qureg);
-  qreal prob = calcTotalProb(qureg);
-  reportScalar("Total probability (3 qubits)", prob);
-
-  DMRAnalytics *analytics_end;
-  dmr_create_custom_analytics_event("circuit_end", &analytics_end);
-  dmr_print_analytics_from(analytics_end);
-  dmr_destroy_custom_analytics_event(analytics_end);
-  destroyQureg(qureg);
+  return createPauliStrSum(strings, coeffs, numQubits);
 }
 
-// Circuit with 4 qubits
-void circuit_4_qubits() {
-
-  Qureg qureg = createQureg(4);
-  reportQuregParams(qureg);
-
+static void run_dynamics_like_circuit(int numQubits) {
+  Qureg qureg = createQureg(numQubits);
+  PauliStrSum hamil = create_heisenberg_ring_hamiltonian(numQubits);
+  PauliStrSum observ = create_alternating_observable(numQubits);
   DMRAnalytics *analytics_start;
-  dmr_create_custom_analytics_event("circuit_start", &analytics_start);
-  dmr_print_analytics_from(analytics_start);
-  dmr_destroy_custom_analytics_event(analytics_start);
-
-  initRandomPureState(qureg);
-  reportQureg(qureg);
-  qreal prob = calcTotalProb(qureg);
-  reportScalar("Total probability (4 qubits)", prob);
-
   DMRAnalytics *analytics_end;
-  dmr_create_custom_analytics_event("circuit_end", &analytics_end);
-  dmr_print_analytics_from(analytics_end);
-  dmr_destroy_custom_analytics_event(analytics_end);
-  destroyQureg(qureg);
-}
-
-// Circuit with 5 qubits
-void circuit_5_qubits() {
-
-  Qureg qureg = createQureg(5);
-  reportQuregParams(qureg);
-
-  DMRAnalytics *analytics_start;
-  dmr_create_custom_analytics_event("circuit_start", &analytics_start);
-  dmr_print_analytics_from(analytics_start);
-  dmr_destroy_custom_analytics_event(analytics_start);
-
-  initRandomPureState(qureg);
-  reportQureg(qureg);
-  qreal prob = calcTotalProb(qureg);
-  reportScalar("Total probability (5 qubits)", prob);
-
-  DMRAnalytics *analytics_end;
-  dmr_create_custom_analytics_event("circuit_end", &analytics_end);
-  dmr_print_analytics_from(analytics_end);
-  dmr_destroy_custom_analytics_event(analytics_end);
-  destroyQureg(qureg);
-}
-
-// Circuit with 6 qubits
-void circuit_6_qubits() {
-
-  DMRAnalytics *analytics_start;
-  Qureg qureg = createQureg(6);
 
   reportQuregParams(qureg);
   dmr_create_custom_analytics_event("circuit_start", &analytics_start);
   dmr_print_analytics_from(analytics_start);
   dmr_destroy_custom_analytics_event(analytics_start);
 
-  initRandomPureState(qureg);
-  reportQureg(qureg);
-  qreal prob = calcTotalProb(qureg);
-  reportScalar("Total probability (6 qubits)", prob);
+  initPlusState(qureg);
 
-  DMRAnalytics *analytics_end;
+  qreal dt = 0.1;
+  int order = 2;
+  int reps = 2;
+  int steps = 4;
+
+  for (int s = 0; s < steps; s++) {
+    applyTrotterizedUnitaryTimeEvolution(qureg, hamil, dt, order, reps);
+  }
+
+  char expecLabel[64];
+  snprintf(expecLabel, sizeof(expecLabel), "<O> after evolution (%d qubits)",
+           numQubits);
+  reportScalar(expecLabel, calcExpecPauliStrSum(qureg, observ));
+
+  char probLabel[64];
+  snprintf(probLabel, sizeof(probLabel), "Total probability (%d qubits)",
+           numQubits);
+  reportScalar(probLabel, calcTotalProb(qureg));
+
   dmr_create_custom_analytics_event("circuit_end", &analytics_end);
   dmr_print_analytics_from(analytics_end);
   dmr_destroy_custom_analytics_event(analytics_end);
+
   destroyQureg(qureg);
+  destroyPauliStrSum(hamil);
+  destroyPauliStrSum(observ);
 }
+
+void circuit_2_qubits() { run_dynamics_like_circuit(2); }
+
+void circuit_3_qubits() { run_dynamics_like_circuit(3); }
+
+void circuit_4_qubits() { run_dynamics_like_circuit(4); }
+
+void circuit_5_qubits() { run_dynamics_like_circuit(5); }
+
+void circuit_6_qubits() { run_dynamics_like_circuit(6); }
 
 int main(int argc, char *argv[]) {
   initQuESTEnv();
